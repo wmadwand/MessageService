889236184b8d4b0e77b5bc68d3fbef94839b60ae
wmadwand <wmadwand@gmail.com>
2019-10-28 15:05:11 +0300

    Improve MessageBus

--- Assets/_Project/Scripts/Core/Helper/Message.cs
+++ Assets/_Project/Scripts/Core/Helper/Message.cs
@@ -2,7 +2,7 @@
 
 namespace Bluehorse.Core.Helper
 {
-    public sealed class Message : IInputMessage, IOutputMessage
+    public sealed class Message : IMessage
     {
         public event Action Receive;
 
@@ -12,7 +12,7 @@ namespace Bluehorse.Core.Helper
         }
     }
 
-    public sealed class Message<T> : IInputMessage<T>, IOutputMessage<T>
+    public sealed class Message<T> : IMessage<T>
     {
         public event Action<T> Receive;
 
@@ -22,7 +22,7 @@ namespace Bluehorse.Core.Helper
         }
     }
 
-    public sealed class Message<T1, T2> : IInputMessage<T1, T2>, IOutputMessage<T1, T2>
+    public sealed class Message<T1, T2> : IMessage<T1, T2>
     {
         public event Action<T1, T2> Receive;
 
@@ -32,7 +32,7 @@ namespace Bluehorse.Core.Helper
         }
     }
 
-    public sealed class Message<T1, T2, T3> : IInputMessage<T1, T2, T3>, IOutputMessage<T1, T2, T3>
+    public sealed class Message<T1, T2, T3> : IMessage<T1, T2, T3>
     {
         public event Action<T1, T2, T3> Receive;
 
@@ -42,7 +42,7 @@ namespace Bluehorse.Core.Helper
         }
     }
 
-    public sealed class Message<T1, T2, T3, T4> : IInputMessage<T1, T2, T3, T4>, IOutputMessage<T1, T2, T3, T4>
+    public sealed class Message<T1, T2, T3, T4> : IMessage<T1, T2, T3, T4>
     {
         public event Action<T1, T2, T3, T4> Receive;
 
--- Assets/_Project/Scripts/MessageBus/MessageBus.cs
+++ Assets/_Project/Scripts/MessageBus/MessageBus.cs
@@ -5,7 +5,9 @@ namespace Bluehorse.Game.Messages
     public sealed class MessageBus
 	{
         public static readonly Message<string, float, object> SampleMessage01 = new Message<string, float, object>();
-	}	
+        public static readonly Message<string, float, object> SampleMessage02 = new Message<string, float, object>();
+        public static readonly Message<string, float, object> SampleMessage03 = new Message<string, float, object>();
+    }	
 
 	public class MessageArgs
 	{
